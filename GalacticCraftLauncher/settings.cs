6ac43e2b9c063dcd778e28bae0a931903f6a2b51
ItzKoolKid96 <polarchanel@gmail.com>
2022-05-28 22:55:03 +0200

    Initial upload

---
+++ GalacticCraftLauncher/settings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GalcticCraftLauncher
+{
+    public class Settings
+    {
+        public string connectionString { get; set; }
+        string connection;
+
+        public void getConnection()
+        {
+            connection = @"Data Source=Settings.db; Version=3";
+            connectionString = connection;
+        }
+    }
+}
